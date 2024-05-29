2f344d5a6a9bbe5e5af699a9df9ae1f7363858ff
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-05-29 14:33:07 +0300

    template join to the controller

--- web/app.js
+++ web/app.js
@@ -7,12 +7,45 @@ rally_app.config(['$routeProvider' ,  $routeProvider =>{
             templateUrl: 'views/welcome.html'
         })
         .when('/rank/general', {
-            templateUrl: 'views/general-rank.html'
+            templateUrl: 'views/general-rank.html',
+            controller: 'FinalRankingController'
         })
         .when('/rank/categorie' , {
-            templateUrl: 'views/categorie-rank.html'
+            templateUrl: 'views/categorie-rank.html',
+            controller: 'CategoryRankController'
         })
         .otherwise({
             redirectTo: '/index'
         });
+}]);
+
+rallyApp.filter('formatTime', function() {
+    return function(totalTime) {
+        const hours = parseInt(totalTime.substring(0, 2));
+        const minutes = parseInt(totalTime.substring(3, 5));
+        const seconds = parseInt(totalTime.substring(6, 8));
+        const milliseconds = parseInt(totalTime.split('.')[1]) / 1000;
+        let timeString = '';
+        if (hours > 0) {
+            timeString += `${hours} heure `;
+        }
+        if (minutes > 0) {
+            timeString += `${minutes} min `;
+        }
+        if (seconds > 0) {
+            timeString += `${seconds} sec `;
+        }
+        if (milliseconds > 0) {
+            timeString += `${milliseconds} millis`;
+        }
+        return timeString.trim();
+    };
+});
+
+rallyApp.controller('FinalRankingController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/rally/api/final-ranking').then(response => $scope.rankings = response.data);
+}]);
+
+rallyApp.controller('CategoryRankController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/rally/api/category-rank').then(response => $scope.rankings = response.data);
 }]);
